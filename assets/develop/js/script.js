136fac3f559664873eb5c46fdcbd94957c8bce8a
eamon <eamon.matheos@gmail.com>
2021-04-09 14:01:00 -0400

    added changes for serch bar

--- assets/develop/js/script.js
+++ assets/develop/js/script.js
@@ -1,3 +1,18 @@
+var sData = [];
+var high_score = localStorage.getItem('cities');
+
+
+if (high_score !== null) {
+    // parse the scores into variable 
+    var sorted_scores = JSON.parse(cities)
+    //itirate over current scores
+    for (x of sorted_scores){
+        $("#high-scores").append('<li class="list-group-item">'+cities+'</li>');
+    }
+    //add current socores to sdata
+    sData = JSON.parse(cities);
+}
+// 
 window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
 window.myWidgetParam.push({id: 11,q: 'quyon',appid: '988b6c911cfcc11d1f0a1b2bfd8472ec',units: 'metric',containerid: 'openweathermap-widget-11',  });  
 (function() {var script = document.createElement('script');
@@ -12,4 +27,21 @@ window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
     script.async = true;
     script.charset = "utf-8";script.src = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
     var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(script, s);  })();
-
+// 
+    $("#save_score").on("click", function(){
+        // get the name/initials and check that input length is higher then 1
+        var get_name=$("#username").val();
+        //validate the input
+        if (get_name.length > 1 ){
+            //craate object from answers
+            var citplace = {
+                citie: get_name.trim(),
+            };
+        //push data to the array sData
+        sData.push(saved_score);
+        // store and json stringify the sData 
+        localStorage.setItem(JSON.stringify(sData));
+        //update the page
+        document.location.href=curUrl;
+        }
+    });
