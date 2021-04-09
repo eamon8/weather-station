7c2d8644a91f7eb0c316b98b1fc32c4d055724a1
eamon <eamon.matheos@gmail.com>
2021-04-09 15:39:45 -0400

    Fix for the city

--- assets/develop/js/script.js
+++ assets/develop/js/script.js
@@ -1,7 +1,18 @@
 var sData = [];
 var high_score = localStorage.getItem('cities');
 
+//get parameters from the url
+var urlParams = new URLSearchParams(window.location.search);
+//get pagebase url
+var pagebase = location.protocol + '//' + location.host + location.pathname
+//city parameter value
+var city = urlParams.get('city');
 
+var city_info_url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=988b6c911cfcc11d1f0a1b2bfd8472ec"
+var c_id;
+$.get(city_info_url, function() {}).done(function(citydata) { 
+    c_id = citydata.id
+});
 if (high_score !== null) {
     // parse the scores into variable 
     var sorted_scores = JSON.parse(cities)
@@ -14,7 +25,7 @@ if (high_score !== null) {
 }
 // 
 window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];  
-window.myWidgetParam.push({id: 11,q: 'quyon',appid: '988b6c911cfcc11d1f0a1b2bfd8472ec',units: 'metric',containerid: 'openweathermap-widget-11',  });  
+window.myWidgetParam.push({id: 11,cityid: c_id,appid: '988b6c911cfcc11d1f0a1b2bfd8472ec',units: 'metric',containerid: 'openweathermap-widget-11',  });  
 (function() {var script = document.createElement('script');
 script.async = true;script.charset = "utf-8";
 script.src = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
@@ -22,7 +33,7 @@ var s = document.getElementsByTagName('script')[0];
 s.parentNode.insertBefore(script, s);  })();
 
 window.myWidgetParam ? window.myWidgetParam : window.myWidgetParam = [];
-  window.myWidgetParam.push({id: 15,q: 'quyon',appid: '988b6c911cfcc11d1f0a1b2bfd8472ec',units: 'metric',containerid: 'openweathermap-widget-15',  });
+  window.myWidgetParam.push({id: 15,cityid: c_id,appid: '988b6c911cfcc11d1f0a1b2bfd8472ec',units: 'metric',containerid: 'openweathermap-widget-15',  });
     (function() {var script = document.createElement('script');
     script.async = true;
     script.charset = "utf-8";script.src = "https://openweathermap.org/themes/openweathermap/assets/vendor/owm/js/weather-widget-generator.js";
